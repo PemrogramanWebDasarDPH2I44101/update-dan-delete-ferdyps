59315bc0657ab50810b80296aad2cc2a89cadcdb
ferdyps <42725335+ferdyps@users.noreply.github.com>
2018-10-04 20:32:30 +0700

    Update form.php

--- form.php
+++ form.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <form action="submit.php" method="post">
-            Nama<input type="text" name="nama" id=""><br>
-            Nim<input type="text" name="nim" id=""><br>
-            Tanggal Lahir<input type="date" name="tgl_lahir" id=""><br>
-            <input type="submit" value="Kirim">
-        </form>
-    </body>
+<html>
+<head>
+    <title>Input Data</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+    Nama : <input type="text" name="nama"><br><br>
+    NIM : <input type="text" name="nim"><br><br>
+    Tanggal Lahir : <input type="date" name="tgl_lahir"><br><br>
+    <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
 </html>
